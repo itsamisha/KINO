5e84cf6f672e143c71e98748a6d9b1d7977957c5
kittocatto1 <anisabinteasad134@gmail.com>
2024-01-23 13:58:47 +0600

    -

--- SQL/home.sql
+++ SQL/home.sql
@@ -1,19 +1,27 @@
 --signin and registration
 SELECT * FROM users WHERE email = $1;
+
 ---category dropdown
 SELECT DISTINCT(category_name) FROM category ORDER BY category_name;
+
 --search
 --by product
 SELECT * FROM product WHERE LOWER(name) LIKE LOWER($1) ORDER BY purchase_count DESC;
+
+
 --category
 SELECT * FROM product p JOIN category c  ON (p.product_id=c.product_id)  
  WHERE LOWER(category_name) LIKE LOWER($1) 
  ORDER BY purchase_count DESC;
+
+
 --seller
 SELECT 	P.* FROM product P JOIN users U
         ON U.user_id = P.user_id 
        WHERE LOWER(U.name) LIKE LOWER($1) AND U.user_type='seller'
    ORDER BY purchase_count DESC;
+
+
 --specific product
 SELECT P.*, U.name AS Shop,
       STRING_AGG(C.category_name, ', ') AS category_name,
@@ -24,8 +32,7 @@ SELECT P.*, U.name AS Shop,
       LEFT JOIN discount D ON D.product_id = P.product_id
       GROUP BY P.product_id, U.name, P.price, D.discount_percentage;
 
---photos
-UPDATE product SET photo_url = $1 WHERE product_id = $2
+
 --change pass
 SELECT * FROM users WHERE user_id = $1;
 UPDATE users SET password = $1 WHERE user_id = $2;
